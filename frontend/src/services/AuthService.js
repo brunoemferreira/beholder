0e7c983da07f10b1d63c8a54ae5fa7c082466ab3
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-20 10:50:53 -0300

    Módulo 1: Introdução > Lição 2: Front Login > 06 – Form Submit - OK

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,12 @@
+export async function doLogin(email, password) {
+   /* const loginUrl = `${API_URL}/login`;
+    const response = await axios.post(loginUrl, { email, password });
+    return response.data; */
+}
+
+export async function doLogout(token) {
+   /* const logoutUrl = `${API_URL}/logout`;
+    const headers = { 'authorization': token };
+    const response = await axios.post(logoutUrl, {}, { headers });
+    return response.data; */
+}
