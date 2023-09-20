e1a739d3f1a24d42ecb239fa8da45f0cda04b48d
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-20 11:57:31 -0300

    Módulo 1: Introdução > Lição 3: Backend >  – Middlewares - OK

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
 
+const cors = require("cors");
+const helmet = require("helmet");
+
 
 
 
 const app = express();
 
+// Middlewares de Configuraçao
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 
-
-
+// Middlewares de Processamento 
 
 
 
