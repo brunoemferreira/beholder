c66a3c4ead323bad6aa03cae9f9250ef0791d343
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-20 21:03:05 -0300

    Módulo 1: Introdução > Lição 4: Segurança > 02 – JWT no Backend - OK

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,10 +1,21 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 function doLogin(req, res, next) {
   const { email, password } = req.body;
-  res.sendStatus(200);
+
+  if (email === "brunoemf@gmail.com" && password === "123456") {
+    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+      expiresIn: parseInt(process.env.JWT_EXPIRES),
+    });
+    res.json({ token });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 function doLogout(req, res, next) {
-    res.sendStatus(200);
+  res.sendStatus(200);
 }
 
 module.exports = { doLogin, doLogout };
